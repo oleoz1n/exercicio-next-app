c0055cd96be700aaaa330d6129246d95fdc04a55
oleoz1n <leopity0@gmail.com>
2023-10-24 18:17:01 -0300

    Editando Cabecalho e page home

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -1,12 +1,19 @@
 import Link from "next/link";
 
 export default function Cabecalho() {
+
   return (
-    <header className='bg-slate-400 h-20 text-white '>
+    <header className='bg-slate-400 h-40 text-white p-2'>
     <nav>
-      <ul>
+      <ul className="flex justify-evenly text-center items-center">
         <li className='text-xl'><Link href="/">HOME</Link></li>
-        <li className='text-xl'><Link href="/">PRODUTOS</Link></li>
+        <div>
+        <li className='text-xl'><Link href="/produtos/camisa">Camisa</Link></li>
+        <li className='text-xl'><Link href="/produtos/calca">Calça</Link></li>
+        <li className='text-xl'><Link href="/produtos/sapato">Sapato</Link></li>
+        <li className='text-xl'><Link href="/produtos/oculos">Óculos</Link></li>
+        <li className='text-xl'><Link href="/produtos/bone">Boné</Link></li>
+        </div>
       </ul>
     </nav>
   </header>
--- src/components/Conteudo/Conteudo.jsx
+++
@@ -1,9 +0,0 @@
-export default function Conteudo() {
-  return (
-    <>
-    <h1>Conheça a nossa loja!</h1>
-    <p>Seja bem-vindo à nossa loja de roupas, onde a moda encontra a sua personalidade.</p>
-    
-    </>
-  )
-}
